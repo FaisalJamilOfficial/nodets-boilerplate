869bfffaa5eb791b599d0d5926bbcaaaf16775a1
Faisal Jamil Khan <133661997+FaisalJamil-DITS@users.noreply.github.com>
2023-10-12 18:54:27 +0500

    Update users.ts

--- src/models/users.ts
+++ src/models/users.ts
@@ -172,7 +172,7 @@ const userSchema = new Schema(
 userSchema.methods.getSignedjwtToken = function () {
   return jwt.sign(
     { _id: this._id, type: this.type },
-    process.env.JWT_SECRET ?? ""
+    process.env.JWT_SECRET || ""
   );
 };
 
