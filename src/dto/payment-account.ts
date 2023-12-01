0577d74f02d09c27fc3841e11bf4b9a709dd8e94
Faisal Jamil Khan <133661997+FaisalJamil-DITS@users.noreply.github.com>
2023-12-01 15:14:08 +0500

    Update payment-account.ts

--- src/dto/payment-account.ts
+++ src/dto/payment-account.ts
@@ -1,12 +1,12 @@
 // file imports
 import { GetElementsDTO } from "./element";
 
-export interface GetPaymentAccountDTO {
+export type GetPaymentAccountDTO = {
   paymentAccount: string;
   user: string;
   key: string;
   value: string;
-}
+};
 
 export interface GetPaymentAccountsDTO extends GetElementsDTO {
   user: string;
