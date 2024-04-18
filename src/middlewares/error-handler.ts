179454bd2dcbd7c545f84f7391b681f44b7f11de
Faisal Jamil Khan <133661997+FaisalJamil-DITS@users.noreply.github.com>
2024-04-18 21:47:41 +0500

    Update error-handler.ts

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -10,30 +10,25 @@ export class ErrorHandler extends Error {
 }
 
 const error = (err: any, _req: Request, res: Response, _next: NextFunction) => {
-  let error = { ...err };
+  let error = err;
 
   console.error(err);
 
   if (err.name === "CastError") {
     const message = `Cast failed for value ${err.value}`;
     error = new ErrorHandler(message, 400);
-  }
-
-  if (err.name === "ValidationError") {
+  } else if (err.name === "ValidationError") {
     const message = Object.values(err.errors).map((e: any) => e.message);
     error = new ErrorHandler(message.toString(), 400);
-  }
-
-  // duplicate value found
-  if (err.code === 11000) {
+  } else if (err.code === 11000) {
     let field = Object.keys(err.keyPattern)[0];
     field = field.charAt(0).toUpperCase() + field.slice(1);
-    const message = `${field} already used, try another one instead!`;
+    const message = `${field} already exists, try another one instead!`;
     error = new ErrorHandler(message, 400);
   }
 
   res.status(Number(error?.statusCode) || 500).json({
-    error: error.message || "Server Error",
+    error: error?.message || "Server Error",
   });
 };
 export default error;
