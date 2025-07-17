6813c04545ba4168a1c2df8b0c3d36ddba22580d
Faisal Jamil <147753822+Dr-FaisalJamil@users.noreply.github.com>
2025-07-18 04:27:08 +0500

    swagger documentation fixed (#8)

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -1,7 +1,8 @@
 // destructuring assignments
-const { LOCAL_3000, ADMIN_PANEL_URL, PIE_HOST_URL } = process.env;
+const { BASE_URL, LOCAL_3000, ADMIN_PANEL_URL, PIE_HOST_URL } = process.env;
 
 export default {
+  BASE_URL: BASE_URL || "",
   LOCAL_3000: LOCAL_3000 || "",
   ADMIN_PANEL_URL: ADMIN_PANEL_URL || "",
   PIE_HOST_URL: PIE_HOST_URL || "",
